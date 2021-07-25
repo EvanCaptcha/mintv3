1fbb337f155b7025d9219c8f3759aae8aed0c2f5
EvanCaptcha <49998003+EvanCaptcha@users.noreply.github.com>
2021-07-25 17:58:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def setInfo1(ses, billSecure, shipSecure, cartId, fName, lName, addy, phone, ema
 
     params = (
         ('avsdata',
-         '{"firstname":"' + fName + '","lastname":"' + lName + '","address1":"' + addy + '","address2":"","city":"Chappaqua","postalCode":"10514","state":"NY","country":"US"}'),
+         '{"firstname":"' + fName + '","lastname":"' + lName + '","address1":"' + addy + '","address2":"","city":"","postalCode":"","state":"NY","country":"US"}'),
     )
 
     response = ses.get('https://catalog.usmint.gov/on/demandware.store/Sites-USM-Site/default/AVS-ajax', headers=headers, params=params)
@@ -103,9 +103,9 @@ def setInfo1(ses, billSecure, shipSecure, cartId, fName, lName, addy, phone, ema
         'dwfrm_billing_billingAddress_emailsource': 'Website - Checkout',
         'dwfrm_singleshipping_shippingAddress_addressFields_address1': f'{addy}',
         'dwfrm_singleshipping_shippingAddress_addressFields_address2': '',
-        'dwfrm_singleshipping_shippingAddress_addressFields_city': 'CHAPPAQUA',
+        'dwfrm_singleshipping_shippingAddress_addressFields_city': '',
         'dwfrm_singleshipping_shippingAddress_addressFields_states_state': 'NY',
-        'dwfrm_singleshipping_shippingAddress_addressFields_zip': '10514-1409',
+        'dwfrm_singleshipping_shippingAddress_addressFields_zip': '',
         'dwfrm_singleshipping_shippingAddress_addressFields_country': 'US',
         'dwfrm_singleshipping_shippingAddress_isCreateAccountSelected': 'false',
         'dwfrm_singleshipping_createAccount_password': '',
@@ -145,9 +145,9 @@ def setCardBill(ses, billSecure, shipSecure, cartId, fName, lName, addy, phone, 
         ('dwfrm_billing_billingAddress_addressFields_lastName', f'{lName}'),
         ('dwfrm_billing_billingAddress_addressFields_address1', f'{addy}'),
         ('dwfrm_billing_billingAddress_addressFields_address2', ''),
-        ('dwfrm_billing_billingAddress_addressFields_city', 'CHAPPAQUA'),
+        ('dwfrm_billing_billingAddress_addressFields_city', ''),
         ('dwfrm_billing_billingAddress_addressFields_states_state', 'NY'),
-        ('dwfrm_billing_billingAddress_addressFields_zip', '10514-1409'),
+        ('dwfrm_billing_billingAddress_addressFields_zip', ''),
         ('dwfrm_billing_billingAddress_addressFields_country', 'US'),
         ('dwfrm_billing_billingAddress_addressFields_phone', f'{phone}'),
         ('dwfrm_billing_billingAddress_email_emailAddress', f'{email}'),
@@ -159,11 +159,11 @@ def setCardBill(ses, billSecure, shipSecure, cartId, fName, lName, addy, phone, 
         ('dwfrm_billing_applyBillingAndPayment', ''),
         ('dwfrm_billing_paymentMethods_selectedPaymentMethodID', 'CREDIT_CARD'),
         ('dwfrm_billing_paymentMethods_creditCard_type', 'Amex'),
-        ('dwfrm_billing_paymentMethods_creditCard_owner', 'Evan Wohl'),
-        ('dwfrm_billing_paymentMethods_creditCard_number', '371756031271032'),
+        ('dwfrm_billing_paymentMethods_creditCard_owner', 'Evan'),
+        ('dwfrm_billing_paymentMethods_creditCard_number', ''),
         ('dwfrm_billing_paymentMethods_creditCard_month', '10'),
-        ('dwfrm_billing_paymentMethods_creditCard_year', '2024'),
-        ('dwfrm_billing_paymentMethods_creditCard_cvn', '0730'),
+        ('dwfrm_billing_paymentMethods_creditCard_year', ''),
+        ('dwfrm_billing_paymentMethods_creditCard_cvn', ''),
         ('dwfrm_emailsignup_phone', ''),
     ]
 
@@ -183,10 +183,10 @@ def setCardBill(ses, billSecure, shipSecure, cartId, fName, lName, addy, phone, 
     data = {
         'dwfrm_billing_paymentMethods_selectedPaymentMethodID': 'CREDIT_CARD',
         'dwfrm_billing_paymentMethods_creditCard_type': 'Amex',
-        'dwfrm_billing_paymentMethods_creditCard_owner': 'Evan Wohl',
-        'dwfrm_billing_paymentMethods_creditCard_number': '*'*11 + '1032',
+        'dwfrm_billing_paymentMethods_creditCard_owner': 'Evan',
+        'dwfrm_billing_paymentMethods_creditCard_number': '*'*11 + '',
         'dwfrm_billing_paymentMethods_creditCard_month': '10',
-        'dwfrm_billing_paymentMethods_creditCard_year': '2024',
+        'dwfrm_billing_paymentMethods_creditCard_year': '',
         'dwfrm_billing_paymentMethods_creditCard_cvn': '****',
         'dwfrm_billing_securekey': f'{billSecure}',
         'dwfrm_emailsignup_phone': ''
@@ -201,7 +201,7 @@ def setCardBill(ses, billSecure, shipSecure, cartId, fName, lName, addy, phone, 
         elif 'Thank you for your order' in response.text:
             print("Possible successful checkout!")
             file = open('checkouts.txt', 'a')
-            webhook = DiscordWebhook(url='https://discord.com/api/webhooks/772848524437487667/xj3K-_QD3iaOaDAUCnEbpv4zraPJkckjMf71FArHEYcNqc4aLEi-5xEW_uMub8nI5RmZ')
+            webhook = DiscordWebhook(url='')
             # create embed object for webhook
             orderNum = 'USM' + response.text.split('<span class="value">USM')[1].split('</span>')[0]
             file.write(email + ':' + orderNum + '\n')
@@ -220,7 +220,7 @@ def setCardBill(ses, billSecure, shipSecure, cartId, fName, lName, addy, phone, 
 
 def monitor(pid):
     global json
-    webhook = DiscordWebhook(url='https://discord.com/api/webhooks/772848524437487667/xj3K-_QD3iaOaDAUCnEbpv4zraPJkckjMf71FArHEYcNqc4aLEi-5xEW_uMub8nI5RmZ')
+    webhook = DiscordWebhook(url='')
     print("Monitoring " + pid + "...")
     try:
         response = requests.get(f"https://catalog.usmint.gov/on/demandware.store/Sites-USM-Site/default/Product-GetAvailability?pid={pid}")
@@ -248,8 +248,8 @@ def task(pid):
     while True:
         try:
             ses = requests.session()
-            addyNum = '21'
-            addyStreet = 'Quaker Lane'
+            addyNum = '2'
+            addyStreet = 'Pond Street'
 
             def random_string_generator(str_size, allowed_chars):
                 return ''.join(random.choice(allowed_chars) for x in range(str_size))
